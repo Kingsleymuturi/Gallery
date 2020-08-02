9fe2c9c57e94c146a04321da311a3aa27b82ced2
Kingsleymuturi <kingsleymuturi9@gmail.com>
2020-08-02 18:32:46 +0300

    initial commit

---
+++ photos/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from .models import Image, Location
+
+
+def index(request):
+    images = Image.objects.all()
+    locations = Location.get_locations()
+    print(locations)
+    return render(request, 'photos/index.html', {'images': images[::-1], 'locations': locations})
+
+
+def image_location(request, location):
+    images = Image.filter_by_location(location)
+    print(images)
+    return render(request, 'photos/location.html', {'location_images': images})
+
+
+def search_results(request):
+    if 'imagesearch' in request.GET and request.GET["imagesearch"]:
+        category = request.GET.get("imagesearch")
+        searched_images = Image.search_by_category(category)
+        message = f"{category}"
+        print(searched_images)
+        return render(request, 'photos/search_results.html', {"message": message, "images": searched_images})
+    else:
+        message = "You haven't searched for any image category"
+        return render(request, 'photos/search_results.html', {"message": message})
